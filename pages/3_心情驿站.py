b5c705c59547c45537c8f756bc600e3a0010e44a
rtt-rzj <rtt_rzj@163.com>
2025-06-02 15:01:09 +0800

    Update 3_心情驿站.py

--- pages/3_心情驿站.py
+++ pages/3_心情驿站.py
@@ -21,4 +21,4 @@ elif selected_mood == "灵感爆发":
 
 st.divider()
 # --- 页脚 ---
-st.markdown("<div class='footer'>© 2025 RONG Zijian | 用 ❤️ 和 Streamlit 构建</div>", unsafe_allow_html=True)
+st.caption("<div class='footer'>© 2025 RONG Zijian | 用 ❤️ 和 Streamlit 构建</div>", unsafe_allow_html=True)
